4bba1d9fa3bfe49169afe988e78bd93f7ecc36fe
Michał Bołtruszko <office@preqdesign.com>
2023-05-15 19:41:30 +0200

    Added models to admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Stadium, Team, Player, Match, Event, Change
+
+admin.site.register(Stadium)
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Match)
+admin.site.register(Event)
+admin.site.register(Change)
