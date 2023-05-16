c99584333de679d2a2a713ed5aaef3a3858e35a9
Michał Bołtruszko <office@preqdesign.com>
2023-05-16 14:15:46 +0200

    Added str to models and added data to stats page

--- main/models.py
+++ main/models.py
@@ -6,12 +6,18 @@ class Stadium(models.Model):
     name = models.CharField(max_length=50)
     localization = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Team(models.Model):
     name = models.CharField(max_length=50)
     manager = models.CharField(max_length=100)
     owner = models.CharField(max_length=100)
     established = models.DateField()
     stadium = models.ForeignKey(Stadium, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Player(models.Model):
     class PositionChoices(models.TextChoices):
@@ -36,12 +42,18 @@ class Player(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     position = models.CharField(max_length=3, choices=PositionChoices.choices, default="SUB")
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 class Match(models.Model):
     host = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='host')
     guest = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='guest')
     stadium = models.ForeignKey(Stadium, on_delete=models.CASCADE)
     date = models.DateField()
     match_round = models.IntegerField()
+
+    def __str__(self):
+        return self.host.name + " - " + self.guest.name + " (kolejka: " + str(self.match_round) + ")" 
 
 class Event(models.Model):
     class EventChoices(models.TextChoices):
@@ -55,14 +67,21 @@ class Event(models.Model):
         OFFSIDE = 'OFFS', _('Spalony')
         CORNER = 'CRNR', _('Rożny')
         FREEKICK = 'FKCK', _('Wolny')
+        ASSIST = 'ASST', _('Asysta')
     
     event_type = models.CharField(max_length=4, choices=EventChoices.choices)
     minute = models.IntegerField()
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     match = models.ForeignKey(Match, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.match) + " (" + str(self.minute) + "') " + self.event_type
+
 class Change(models.Model):
     in_player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player_in')
     out_player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player_out')
     minute = models.IntegerField()
     match = models.ForeignKey(Match, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.match) + " (" + str(self.in_player.team) + " - " + str(self.in_player) + ")"
--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ import genericpath
 from django.shortcuts import get_object_or_404, render
 
 from .models import Match, Team, Player, Event
-from django.db.models import Count 
+from django.db.models import Count, Q
     
 def matches(request):
     teamsLength = Team.objects.count()
@@ -19,19 +19,55 @@ def match(request, match_id):
     return render(request, "match.html", {"match": match_id})
 
 def table(request):
-    tteams = list(Team.objects.all().values_list('name', flat=True)) 
+    tteams = list(Team.objects.values_list('name', "id"))
+    events = list(Event.objects.values_list("event_type", "player"))
     print(tteams)
+    print(events)
+    TT = [];
+
+    matches = list(Match.objects.all())
+
+    def calcData(matches, TT):
+        for match in matches:
+
+            eventsTT = list(Event.objects.values_list("event_type", "match").filter(Q(event_type='GOAL') | Q(event_type='GPEN')).filter(pk=match.id))
+            print(eventsTT)
+
+    # match for 2 teams, only goals, add to 2 teams calc data (pts, w/d/l)
+
+
+    calcData(matches, TT)
+
     teams = [1,2,3]
     return render(request, "table.html", {"teams": teams})
 
 def statistics(request):
-    # goalss = list(Event.objects.values_list("event_type", "player").filter(event_type = 'GOAL').values('player').annotate(total=Count('player')).order_by('-total'))
-    goalss = Player.objects.annotate(List=Count('event'), total=Count('event__event_type'))
-    print(goalss)
-    goals = [1,2,3,4,5]
-    assists = [1,2,3,4,5]
-    yellows = [1,2,3,4,5]
-    reds = [1,2,3,4]
+    players = list(Player.objects.all())
+
+    goalsEvents = list(Event.objects.values_list("event_type", "player").filter(event_type = 'GOAL').values('player').annotate(total=Count('player')).order_by('-total'))[:5]
+    goals = []
+
+    for x in goalsEvents:
+        goals.append(dict(total = x["total"], player = players[x["player"] - 1], index = goalsEvents.index(x) + 1))
+
+    assistsEvents = list(Event.objects.values_list("event_type", "player").filter(event_type = 'ASST').values('player').annotate(total=Count('player')).order_by('-total'))[:5]
+    assists = []
+
+    for x in assistsEvents:
+        assists.append(dict(total = x["total"], player = players[x["player"] - 1], index = assistsEvents.index(x) + 1))
+
+    yellowsEvents = list(Event.objects.values_list("event_type", "player").filter(event_type = 'YCAR').values('player').annotate(total=Count('player')).order_by('-total'))[:5]
+    yellows = []
+
+    for x in yellowsEvents:
+        yellows.append(dict(total = x["total"], player = players[x["player"] - 1], index = yellowsEvents.index(x) + 1))
+
+    redsEvents = list(Event.objects.values_list("event_type", "player").filter(event_type = 'RCAR').values('player').annotate(total=Count('player')).order_by('-total'))[:5]
+    reds = []
+
+    for x in redsEvents:
+        reds.append(dict(total = x["total"], player = players[x["player"] - 1], index = redsEvents.index(x) + 1))
+        
     return render(request, "statistics.html", {"goals": goals, "assists": assists, "yellows": yellows, "reds": reds})
 
 def club(request, club_id):
