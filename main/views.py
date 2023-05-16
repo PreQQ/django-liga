16ba81fd999c2d6e9ba6f76cb749af8247b2ca8f
Michał Bołtruszko <office@preqdesign.com>
2023-05-16 02:55:53 +0200

    Initialzed queries to db in stats

--- main/views.py
+++ main/views.py
@@ -1,12 +1,18 @@
 import genericpath
 from django.shortcuts import get_object_or_404, render
 
-from .models import Match, Team, Player
+from .models import Match, Team, Player, Event
+from django.db.models import Count 
     
 def matches(request):
+    teamsLength = Team.objects.count()
+    rowsMax = (teamsLength - 1) * 2
+
+    matchess = Match.objects.all()
+    print(matchess)
     rows = [1,2,3,4,5]
     matches = [1,2,3,4,5]
-    return render(request, "matches.html", {"matches": matches, "rows": rows})
+    return render(request, "matches.html", {"matches": matches, "rows": rows, "rowsMax": rowsMax})
 
 def match(request, match_id):
     # matchFinder = get_object_or_404(Match, pk=match_id)
@@ -19,6 +25,9 @@ def table(request):
     return render(request, "table.html", {"teams": teams})
 
 def statistics(request):
+    # goalss = list(Event.objects.values_list("event_type", "player").filter(event_type = 'GOAL').values('player').annotate(total=Count('player')).order_by('-total'))
+    goalss = Player.objects.annotate(List=Count('event'), total=Count('event__event_type'))
+    print(goalss)
     goals = [1,2,3,4,5]
     assists = [1,2,3,4,5]
     yellows = [1,2,3,4,5]
@@ -27,7 +36,7 @@ def statistics(request):
 
 def club(request, club_id):
     team = get_object_or_404(Team, pk=club_id)
-    players = team.player_set.all()
+    players = list(team.player_set.all())
     rows = [1,2,3,4,5]
     matches = [1,2,3,4,5]
     return render(request, "club.html", {"team": team, "players": players, "matches": matches, "rows": rows})
@@ -35,4 +44,3 @@ def club(request, club_id):
 def player(request, player_id):
     player = get_object_or_404(Player, pk=player_id)
     return render(request, "player.html", {"player": player})
-    # return render(request, "match.html", {"match": player})
